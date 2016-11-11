3ea9dabb12a869f33e497bbafa77ea046be785e3
Carlos Arroyo <coarroyo30@gmail.com>
2016-11-11 15:36:02 -0500

    Add files via upload

---
+++ Interactive Photo Gallery/js/app.js
@@ -0,0 +1,75 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+var $leftArrow = $("<div id='leftArrow'></div>");
+var $rightArrow = $("<div id='rightArrow'></div>");
+var $closeLightbox = $("<div id='closeLightbox'></div><div style='clear:both'></div>");
+
+//An image to overlay
+$overlay.append($image);
+
+$image.before($closeLightbox);
+$image.before($leftArrow);
+$image.after($rightArrow);
+
+//add caption
+$overlay.append($caption);
+
+//add the overlay
+$("body").append($overlay);
+  
+//Capture the click event on a link to an image
+$(".images a").click(function(event) {
+  event.preventDefault();
+  
+  getCurrentImage(this);
+   
+  //Show the overlay.
+  $overlay.show();
+});
+
+$leftArrow.click(function(){
+  getPrevImage();
+});
+
+$rightArrow.click(function(){
+  getNextImage();
+});
+
+function getCurrentImage (currentImage) {  
+  thisImage = currentImage;
+  var imageLocation = $(currentImage).attr("href");
+    
+  //Update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+
+  //Get child's alt attribute and set caption
+  var captionText = $(currentImage).children("img").attr("title");
+  $caption.text(captionText);
+}
+
+function getPrevImage() {
+  imageParent = $(thisImage).parent().prev();
+  if(imageParent.length!=0){
+      thisImage = $(imageParent).children("a");
+    // imageLocation = $(thisImage).attr("href");
+    // $image.attr("src", imageLocation);
+  }
+  getCurrentImage(thisImage);    
+}
+
+function getNextImage() {
+  imageParent = $(thisImage).parent().next();
+  if(imageParent.length!=0){
+    thisImage = $(imageParent).children("a");
+    // imageLocation = $(thisImage).attr("href");
+    // $image.attr("src", imageLocation);
+  }
+  getCurrentImage(thisImage);
+}
+
+//When overlay is clicked
+$closeLightbox.click(function(){
+  //Hide the overlay
+  $overlay.hide();
+});
