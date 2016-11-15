51f57cc0a1f00397244e1ef3558976cf25f50858
Carlos Arroyo <coarroyo30@gmail.com>
2016-11-15 10:36:49 -0500

    Update app.js

--- Interactive Photo Gallery/js/app.js
+++ Interactive Photo Gallery/js/app.js
@@ -75,3 +75,34 @@ $closeLightbox.click(function(){
   //Hide the overlay
   $overlay.hide();
 });
+
+//search input
+(function() {
+  var $imgs = $(".images img");
+  var $search = $("#input");
+  var cache = [];
+  
+  $imgs.each(function() {
+    cache.push({
+      element: this,
+      text: this.alt.trim().toLowerCase()
+    });
+  });
+  function filter() {
+    var query = this.value.trim().toLowerCase();
+    
+    cache.forEach(function(img) {
+      var index = 0;
+      if (query) {
+        index = img.text.indexOf(query);
+      }
+      img.element.style.display = index === -1 ? 'none' : '';
+    });
+  }
+    
+    if ('oninput' in $search[0]) {
+        $search.on('input', filter);
+    } else {
+      $search.on('keyup', filter);
+    }
+}());
